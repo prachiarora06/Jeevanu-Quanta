e9e7eecd1322b21d06b64bc0f03fbd79703c15d3
rynsf <rynsf@users.noreply.github.com>
2024-06-10 12:54:37 +0530

    Add TopAppBar

--- app/src/main/java/com/prachiarora06/jeevanu_quanta/AppHome.kt
+++ app/src/main/java/com/prachiarora06/jeevanu_quanta/AppHome.kt
@@ -4,13 +4,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppHome() {
     Scaffold(
@@ -18,10 +25,28 @@ fun AppHome() {
             FloatingActionButton(onClick = { /*TODO*/ }) {
                 Icon(
                     Icons.Filled.Add,
-                    contentDescription = "Add Image Button")
+                    contentDescription = "Add Image Button"
+                )
             }
         },
-        modifier = Modifier.fillMaxSize()) { innerPadding ->
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Jeevanu Quanta")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.MoreVert,
+                            contentDescription = "Menu")
+                    }
+                })
+        },
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
         Text(
             "Hello World!",
             modifier = Modifier.padding(innerPadding)
